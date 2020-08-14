16451866dbf59bb7ecff6ed9f5722a1961162865
gabays <gabay.simon@gmail.com>
2020-08-14 10:14:13 +0200

    train 3 - revised

--- Models/Train_3/_commands.sh
+++ Models/Train_3/_commands.sh
@@ -1,5 +1,7 @@
 #check third column
 cut -f3 lemmat_full.tsv > colonne3.tsv
+#remove lines in greek
+sed -i "" '/.*[αβγδεζηθικλμυνξπρσςτφχψωΩΨΦΣΠΞΛΘΔἡῷάίὶὁἢ].*/d' lemmat_full.tsv
 #protogénie
 protogenie build lemmat_full.xml -d 0.01 -e 0.05;
 protogenie build pos_full.xml -d 0.03 -e 0.1;
